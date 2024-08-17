b8187455b11ea05a42ef4b0277bf59a49994dfa2
Sri Adithya Sridhar <addithyasri@gmail.com>
2024-08-17 17:25:20 +0530

    INTIALIZING

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
-const portfolio = {
+/*const portfolio = {
     name : "Cristiano Adi",
     phone : 7619490166,
     follow : true,
 }
 
 console.dir(typeof portfolio ['name'])
+
+let score = prompt("Enter the score")
+score = parseFloat(score)
+if ( score >= 80 && score <=100 ) {
+    console.log("Grade A");
+}
+else if ( score >=70 && score <= 79 ) {
+    console.log("Grade B");
+}
+else if ( score >=60 && score <= 69 ) {
+    console.log("Grade C");
+}
+else if ( score >= 50 && score <= 59 ) {
+    console.log("Grade D");
+}
+else if( score >= 40 && score <= 49 ) {
+    console.log("Fail");
+}
+else {
+    console.log("Invalid Input");
+}
+console.log(score)
+
+
+
+
+digit = prompt("Enter the number");
+if ( digit % 5 == 0) {
+    console.log("Its a multiple of 5");
+}
+else {
+console.log("Its not a multiple");
+}   */
+
+OIBSIP-Temperature-Converter-Website-
+
+
